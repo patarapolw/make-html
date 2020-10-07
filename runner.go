80e07327b0e98abf37c3071cc96eddcb1ae3380a
Pacharapol Withayasakpunt <polv@polv.cc>
2020-10-07 23:04:59 +0700

    :ambulance: (runner.go) update desktop runner

--- runner.go
+++ runner.go
@@ -4,23 +4,16 @@ import (
 	"log"
 	"os/exec"
 	"runtime"
-	"strings"
 )
 
 func main() {
 	cmdArgs := []string{"-jar"}
 
-	platform := "gtk.linux.x86_64"
-
-	switch runtime.GOOS {
-	case "windows":
-		platform = "win32.win32.x86_64"
-	case "darwin":
-		platform = "cocoa.macosx.x86_64"
+	if runtime.GOOS == "darwin" {
 		cmdArgs = append(cmdArgs, "-XstartOnFirstThread")
 	}
 
-	cmdArgs = append(cmdArgs, strings.Join([]string{"makehtml-", platform, ".jar"}, ""))
+	cmdArgs = append(cmdArgs, "makehtml.jar")
 
 	err := exec.Command(
 		"java",
